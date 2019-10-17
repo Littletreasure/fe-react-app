fa1f6238bc1244871cc9fa8dead706baa6b7e651
ChristopherMills <christopher.mills.github@outlook.com>
2019-10-17 14:19:10 +0100

    hbvguyvuyv

--- src/grid.jsx
+++ src/grid.jsx
@@ -1,1 +1,36 @@
 import React from 'react';
+import './App.css';
+
+const Grid = (props) => {
+  return (
+    <div className="gridContainer">
+      <div key="1">
+        <button></button>
+      </div>
+      <div key="2">
+        <button></button>
+      </div>
+      <div key="3">
+        <button></button>
+      </div>
+      <div key="4">
+        <button></button>
+      </div>
+      <div key="5">
+        <button></button>
+      </div>
+      <div key="6">
+        <button></button>
+      </div>
+      <div key="7">
+        <button></button>
+      </div>
+      <div key="8">
+        <button></button>
+      </div>
+      <div key="9">
+        <button></button>
+      </div>
+    </div>
+  );
+};
